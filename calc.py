6e964b8ed314e93568eee93f42fc515c1c9479d0
swapnil085 <sbuchke085@gmail.com>
2020-04-17 10:57:51 +0530

    sum,add,multiply added

---
+++ calc.py
@@ -0,0 +1,16 @@
+def sum(a,b):
+    return a+b
+
+def diff(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a//b
+
+if __name__ == "__main__":
+    print(sum(4,2))
+    print(diff(4,2))
+    print(multiply(4,2)) 
